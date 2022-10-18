03331cdf93c0a75279f95ff6f9994bedb1443517
oscarthecoderr <dev.oscarrios@gmail.com>
2022-10-17 23:24:53 -0400

    fixed the key and is ready to run. css is next

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
-document.querySelector('button').addEventListener('click',runIt)
+document.querySelector("button").addEventListener("click", runIt);
 
-function runIt(){
-let choice = document.querySelector('input').value
-console.log(choice)
-let url = `https://superheroapi.com/api.php/10217557089079430/search/${choice}`
-fetch(url)
-.then (res => res.json())
-.then((data) => {
-  console.log(data)
+function runIt() {
+  let choice = document.querySelector("input").value;
+ // console.log(choice);
+  let url = `https://superheroapi.com/api.php/10217557089079430/search/${choice}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector("h2").innerText =
+        data.results[0].biography.publisher;
+      document.querySelector("h3").innerText = data.results[0].powerstats.speed;
+      document.querySelector("img").src = data.results[0].image.url;
 
-  document.querySelector('h2').innerText = data.results[0].biography.publisher
-  document.querySelector('h3').innerText= data.results[0].powerstats.speed
-  document.querySelector('img').src = data.results[0].image.url
-
-
-
-let word = data.results[0].biography.publisher
-fetch(`http://www.omdapi.com/?apikey=74289840&t=${word}`)
-.then(res => res.json)
-.then((genre) => {
-  console.log(genre)
-  document.querySelector('p').innerText = data.genre
-})
-
-
-})
-
+      let word = data.results[0].biography.publisher;
+      console.log(word);
+      fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=2fd7d557&t=${word}`)
+        .then(res => res.json())
+        .then((data) => {
+          console.log(data, "test");
+          document.querySelector('p').innerText = data.Genre
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
 }
-
 
 /// work on it with simeon. really fun
